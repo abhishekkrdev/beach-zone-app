4276feb1db9b946cf6fc242026b9f725da37c6a9
Abhishek Kumar <abhishekkr95.dev@gmail.com>
2020-10-02 21:21:58 +0530

    Folder Structure

---
+++ src/components/home/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <header> Hello from Header</header>
+    </React.Fragment>
+  );
+};
+
+export default Header;
